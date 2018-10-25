fee5f3307537efc14a9e2379e5bfc03a37465205
Enter <350840291@qq.com>
2018-10-25 16:28:35 +0800

    sql

--- src/Wcoder.Blog.InjectionConfig/ManagedInjection.cs
+++ src/Wcoder.Blog.InjectionConfig/ManagedInjection.cs
@@ -4,8 +4,16 @@ namespace Wcoder.Blog.InjectionConfig
 {
     public static class ManagedInjection
     {
-        public static void AddWcoderBlogServices(this IServiceCollection serviceCollection)
+        public static void AddWcoderBlogServices(this IServiceCollection services)
         {
+            var sqlConnectionString = Configuration.GetConnectionString("DataAccessMsSqlServerProvider");
+
+            services.AddDbContext<DomainModelMsSqlServerContext>(options =>
+                options.UseSqlServer(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("AspNetCoreMultipleProject")
+                )
+            );
         }
     }
 }
