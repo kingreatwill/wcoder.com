f74f5740b9d5ffa3814106bdc54b4729d3f072c2
Enter <350840291@qq.com>
2018-10-27 09:05:25 +0800

    blazor

--- src/Dashboard/Wcoder.Blog.Dashboard.Server/Startup.cs
+++ src/Dashboard/Wcoder.Blog.Dashboard.Server/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
 using System.Net.Mime;
@@ -12,12 +13,19 @@ namespace Wcoder.Blog.Dashboard.Server
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
-
+            //services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
+            services.AddWcoderBlogServices(Configuration);
             // Adds the Server-Side Blazor services, and those registered by the app project's startup.
             services.AddServerSideBlazor<App.Startup>();
 
