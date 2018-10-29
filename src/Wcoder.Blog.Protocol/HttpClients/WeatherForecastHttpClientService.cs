8d15e0d14eca9019cf41dc48e918683a47bc064f
Enter <350840291@qq.com>
2018-10-29 18:18:29 +0800

    remove polly

--- src/Wcoder.Blog.Protocol/HttpClients/WeatherForecastHttpClientService.cs
+++ src/Wcoder.Blog.Protocol/HttpClients/WeatherForecastHttpClientService.cs
@@ -11,23 +11,42 @@ namespace Wcoder.Blog.Protocol.HttpClients
     public class WeatherForecastHttpClientService : IWeatherForecastService
     {
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly HttpClient httpClient;
         private readonly string controllerName = "WeatherForecastService";
         private readonly string clientName = "WcoderBlog";
 
-        public WeatherForecastHttpClientService(IHttpClientFactory httpClientFactory)
+        public WeatherForecastHttpClientService(IHttpClientFactory httpClientFactory, HttpClient httpClient)
         {
             this.httpClientFactory = httpClientFactory;
+            this.httpClient = httpClient;
         }
 
         public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            var url = $"/{controllerName}/{nameof(GetForecastAsync)}";
-            var client = httpClientFactory.CreateClient(clientName);
-            var r = await client.GetAsync(url);
-            using (var stream = await r.Content.ReadAsStreamAsync())
+            try
             {
-                DataContractJsonSerializer jsonSerialize = new DataContractJsonSerializer(typeof(WeatherForecast[]));
-                return (WeatherForecast[])jsonSerialize.ReadObject(stream);
+                var url = $"/{controllerName}/{nameof(GetForecastAsync)}";
+                var client = httpClientFactory.CreateClient(clientName);
+                var r = await client.GetAsync(url);
+
+                using (var stream = await r.Content.ReadAsStreamAsync())
+                {
+                    DataContractJsonSerializer jsonSerialize = new DataContractJsonSerializer(typeof(WeatherForecast[]));
+                    return (WeatherForecast[])jsonSerialize.ReadObject(stream);
+                }
+            }
+            catch (Exception er)
+            {
+                // await Microsoft.JSInterop.JSRuntime.Current.InvokeAsync<string>("console.log", er.StackTrace);
+                //window.console.log
+                return new WeatherForecast[] {
+                    new WeatherForecast{
+                        Date=DateTime.Now,
+                        Summary= httpClientFactory==null? "sdf":httpClientFactory.ToString(),
+                        TemperatureC=1,
+                        TemperatureF=2
+                    }
+                };
             }
         }
     }
--- src/Wcoder.Blog.Protocol/HttpClients/WebApiClientProxyExtensions.cs
+++ src/Wcoder.Blog.Protocol/HttpClients/WebApiClientProxyExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Polly;
-using System;
+﻿using System;
 using Wcoder.Blog.Protocol.HttpClients;
 using Wcoder.Blog.Protocol.Interfaces;
 namespace Microsoft.Extensions.DependencyInjection
@@ -12,12 +11,7 @@ namespace Microsoft.Extensions.DependencyInjection
             {
                 client.BaseAddress = new Uri("http://localhost:5003");
                 client.DefaultRequestHeaders.Add("Accept", "application/json");
-            }).AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
-            {
-                TimeSpan.FromSeconds(1),
-                TimeSpan.FromSeconds(2),
-                TimeSpan.FromSeconds(3)
-            }));
+            });
 
             // Services
 			services.AddScoped<IWcoderBlogService, WcoderBlogHttpClientService>();
