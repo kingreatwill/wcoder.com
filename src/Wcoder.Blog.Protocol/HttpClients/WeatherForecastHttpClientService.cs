19a7d6b3aeeb2927b96b113549684092e9a93b8b
Enter <350840291@qq.com>
2018-10-29 19:03:30 +0800

    ServiceStack.Text

--- src/Wcoder.Blog.Protocol/HttpClients/WeatherForecastHttpClientService.cs
+++ src/Wcoder.Blog.Protocol/HttpClients/WeatherForecastHttpClientService.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Linq;
 using System.Net.Http;
-using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using Wcoder.Blog.Protocol.Interfaces;
 using Wcoder.Blog.Protocol.Models;
@@ -23,30 +21,13 @@ namespace Wcoder.Blog.Protocol.HttpClients
 
         public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            try
-            {
-                var url = $"/{controllerName}/{nameof(GetForecastAsync)}";
-                var client = httpClientFactory.CreateClient(clientName);
-                var r = await client.GetAsync(url);
+            var url = $"/{controllerName}/{nameof(GetForecastAsync)}";
+            var client = httpClientFactory.CreateClient(clientName);
+            var r = await client.GetAsync(url);
 
-                using (var stream = await r.Content.ReadAsStreamAsync())
-                {
-                    DataContractJsonSerializer jsonSerialize = new DataContractJsonSerializer(typeof(WeatherForecast[]));
-                    return (WeatherForecast[])jsonSerialize.ReadObject(stream);
-                }
-            }
-            catch (Exception er)
+            using (var stream = await r.Content.ReadAsStreamAsync())
             {
-                // await Microsoft.JSInterop.JSRuntime.Current.InvokeAsync<string>("console.log", er.StackTrace);
-                //window.console.log
-                return new WeatherForecast[] {
-                    new WeatherForecast{
-                        Date=DateTime.Now,
-                        Summary= httpClientFactory==null? "sdf":httpClientFactory.ToString(),
-                        TemperatureC=1,
-                        TemperatureF=2
-                    }
-                };
+                return await ServiceStack.Text.JsonSerializer.DeserializeFromStreamAsync<WeatherForecast[]>(stream);
             }
         }
     }
